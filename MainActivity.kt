718af9cfcc7033090e9d9edb1f8bd9c17ae0f8ec
Rikned1223 <110538989+Rikned1223@users.noreply.github.com>
2022-12-22 15:41:08 +0600

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.task
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.task.data.Pref
+import com.example.task.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val pref = Pref(this)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+
+        if (!pref.isOnBoardingSeen())
+            navController.navigate(R.id.navigation_on_boarding)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home,
+                R.id.navigation_dashboard,
+                R.id.navigation_notifications,
+                R.id.navigation_profile,
+                R.id.navigation_task
+            )
+        )
+        val navFragments = arrayListOf(
+            R.id.navigation_home,
+            R.id.navigation_dashboard,
+            R.id.navigation_notifications,
+            R.id.navigation_profile,
+        )
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            navView.isVisible = navFragments.contains(destination.id)
+            if (destination.id == R.id.navigation_on_boarding) {
+                supportActionBar?.hide()
+            } else supportActionBar?.show()
+        }
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
---
+++ ProfileFragment.kt
@@ -0,0 +1,78 @@
+package com.example.task.ui.profile
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
+import com.example.task.data.Pref
+import com.example.task.databinding.FragmentProfileBinding
+import com.example.task.extension.loadImage
+
+class ProfileFragment : Fragment() {
+
+    private lateinit var pref: Pref
+    private lateinit var binding: FragmentProfileBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        clickListener()
+    }
+
+
+    private fun clickListener() {
+        pref = Pref(requireContext())
+        //profile photo
+        binding.ivProfile.loadImage(pref.getImage())
+        binding.ivProfile.setOnClickListener {
+            val intent = Intent()
+            intent.type = "image/*"
+            intent.action = Intent.ACTION_GET_CONTENT
+            launcher.launch(intent)
+        }
+        //edit text
+        binding.etUserAge.setText(pref.getAge())
+        binding.etUserAge.addTextChangedListener {
+            pref.setAge(binding.etUserAge.text.toString())
+        }
+        //edit text
+        binding.etUserGen.setText(pref.getGen())
+        binding.etUserGen.addTextChangedListener {
+            pref.setGen(binding.etUserGen.text.toString())
+        }
+        //edit text
+        binding.etUserName.setText(pref.getName())
+        binding.etUserName.addTextChangedListener {
+            pref.setName(binding.etUserName.text.toString())
+        }
+    }
+
+    //set profile photo
+    private val launcher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result: ActivityResult ->
+        if (result.resultCode == Activity.RESULT_OK
+            && result.data != null
+        ) {
+            val photoUri: Uri? = result.data?.data
+            pref.setImage(photoUri.toString())
+            binding.ivProfile.loadImage(photoUri.toString())
+        }
+    }
+}
